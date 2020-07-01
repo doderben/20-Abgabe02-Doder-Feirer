4d88eb2cb80421c113e98219bee708527ab56cad
DavidFeirer <david.feirer@edu.campus02.at>
2020-07-01 11:11:26 +0200

    updated pom.xml

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -3,10 +3,15 @@ package at.fhj.iit;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Testing for the class String Queue
+ */
+@DisplayName("Testing StringQueue")
 class StringQueueTest {
-
+    private StringQueue a;
    // TODO Write your own tests
 
 }
